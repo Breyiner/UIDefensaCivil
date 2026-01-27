74e527e649e5a033b067c24c01a784c22c1a0a4d
EdixonDCT <edixondelta2022@gmail.com>
2026-01-27 16:46:42 -0500

    arreglos iconos menu

--- src/Views/VerPlanFamiliar/verPlanController.js
+++ src/Views/VerPlanFamiliar/verPlanController.js
@@ -25,19 +25,27 @@ export default async () => {
     if (!evaluacion)
     {
         await paginacion();
-        await cargarPagina();   
+        await cargarPagina();
+        window.procesoPeticion = false
     }
     else{
         container.innerHTML = `<div class="noPlan">No tienes ningun plan familiar realizado.</div>`
+        window.procesoPeticion = false;
     }
 
-    containerPaginador.addEventListener("click", async (e) => {
-        if (!e.target.classList.contains("paginador__numero")) return;
-        paginaActual = e.target.id;
-        paginacion();
-        cargarPagina();
-    });
-
+    containerPaginador.addEventListener("click", async (e) => {   
+        if (e.target.classList.contains("paginador__numero") && !window.procesoPeticion)
+        {
+            paginaActual = e.target.id;
+            paginacion();
+            cargarPagina();
+        }});
+    container.addEventListener("click", async (e) => {
+        if (e.target.classList.contains("verPlan__boton") && !window.procesoPeticion) 
+        {
+            location.href = `#/verPlanFamiliar/menu/id=${e.target.id}`;
+        }
+    })
   
     async function paginacion() {
         containerPaginador.innerHTML = "";
@@ -53,16 +61,27 @@ export default async () => {
             }
         }
         else{
-            // let numeroCasillas = 0;
-            // let numeroEmpieza = 0
-            // if (cantidad-paginaActual) {
-            //     numeroCasillas = paginaActual;
-            //     numeroEmpieza = cantidad-paginaActual;
-            // }
-            // else{
-            //     numeroCasillas = 10;
-            //     numeroEmpieza = 1;
-            // }
+            const botonAtras = document.createElement("button");
+            botonAtras.classList.add("paginador__numero");
+            botonAtras.id = paginaActual != 1 ? Number(paginaActual)-1 : paginaActual;
+            botonAtras.innerHTML = `<i class="ri-arrow-left-wide-line"></i>`
+            containerPaginador.appendChild(botonAtras);
+            let numeroCasillas = 0;
+            let numeroEmpieza = 0
+            if (paginaActual != 1)
+            {
+                numeroEmpieza = paginaActual;
+                numeroCasillas = Number(numeroEmpieza) + 9;
+                if (numeroCasillas > cantidad)
+                {
+                    numeroCasillas = cantidad;
+                    numeroEmpieza = Number(cantidad)-9 
+                }
+            }
+            else{
+                numeroCasillas = 10;
+                numeroEmpieza = 1;
+            }
             for (numeroEmpieza; numeroEmpieza <= numeroCasillas; numeroEmpieza++){
                 const contenedor = document.createElement("button");
                 contenedor.classList.add("paginador__numero");
@@ -71,6 +90,11 @@ export default async () => {
                 contenedor.textContent = numeroEmpieza;
                 containerPaginador.appendChild(contenedor);
             }
+            const botonSiguiente = document.createElement("button");
+            botonSiguiente.classList.add("paginador__numero");
+            botonSiguiente.id = (Number(paginaActual)+1) > cantidad ? cantidad : Number(paginaActual)+1;
+            botonSiguiente.innerHTML = `<i class="ri-arrow-right-wide-line"></i>`;
+            containerPaginador.appendChild(botonSiguiente);
         }
     }
 
@@ -83,26 +107,20 @@ export default async () => {
                 let info = planes[plan];
                 let cartaInfo = document.createElement('div');
                 cartaInfo.innerHTML = `
-                <div class="verPlan">
                     <div class="verPlan__apellidoEstado">
                         <div class="verPlan__apellidos"><i class="ri-parent-fill"></i>   ${info.family_plan.last_names}</div>
                         <div class="verPlan__estado verPlan__estado--naranja">${info.action.name}</div>
                     </div>
                     <div class="verPlan__detalles">
-                        <i class="ri-map-pin-2-fill"></i> ${info.family_plan.city.apartment.name} - ${info.family_plan.city.name}
+                        <i class="ri-map-pin-line"></i> ${info.family_plan.city.apartment.name} - ${info.family_plan.city.name}
                     </div>
                     <div class="verPlan__detalles">
-                        <i class="ri-calendar-fill"></i> Ultima Edicion: ${info.date}
+                        <i class="ri-calendar-event-fill"></i> Ultima Edicion: ${info.date}
                     </div>
-                    <button class="verPlan__boton boton" id=${info.family_plan.id}>Revisar Plan</button>
-                </div>`;
+                    <button class="verPlan__boton boton" id=${info.family_plan.id}>Revisar Plan</button>`;
+                cartaInfo.classList.add('verPlan');
                 container.appendChild(cartaInfo);
         }
-    
-        containerPaginador.addEventListener("click", async (e) => {
-        if (!e.target.classList.contains("verPlan__boton")) return;
-            location.href = `#/verPlanFamiliar/menu/id=${e.target.id}`;
-        });
     }
 
     async function evaluarDatos()
