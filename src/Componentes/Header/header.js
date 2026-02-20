7cb1b238d278d622cfae2edbf1eb21fb92cda8b9
Valentina serrano <valentinaserrano120@gmail.com>
2026-02-20 15:44:21 -0500

    Se actualizo el header

--- src/Componentes/Header/header.js
+++ src/Componentes/Header/header.js
@@ -4,39 +4,6 @@ const listConNoti = ['home']
 const listSinBack = ['home','supervisorHomepage/ver']
 
 export const componenteHeader = () => { 
-    const header = document.querySelector(".header");
-    const noti = document.querySelector(".header__notification");
-    const burbuja = document.querySelector(".header__burbuja");
-    const numerito = document.querySelector(".header__numerito");
-    const back = document.querySelector(".header__boton");
-    const hash = location.hash.slice(2);
 
-    if(!cookie.existe()){
-        header.classList.add("invisible");
-    }
-    else{
-        header.classList.remove("invisible");
-    }
-    if (listConNoti.includes(hash))
-    {
-        noti.classList.remove('invisible');
-        let peticion = 2;
-        if (peticion > 1)
-        {
-            burbuja.classList.remove('invisible');   
-            numerito.textContent = peticion;
-        }
-    }
-    else{
-        noti.classList.add('invisible');
-        burbuja.classList.add('invisible');       
-    }
-    if(listSinBack.includes(hash))
-    {
-        back.classList.add('invisible');
-    }
-    else
-    {
-        back.classList.remove('invisible');
-    }
 };
+
