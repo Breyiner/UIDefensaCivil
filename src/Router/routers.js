8b047c73cd1f6b1b3285df4b236b25ed8061d30d
sfbtm <dilansanv033@gmail.com>
2026-03-02 16:04:44 -0500

    Carpetas borradas, main.js solo estilos - Evitar conflictos

--- src/Router/routers.js
+++
@@ -1,9 +0,0 @@
-import { loginController } from '../Views/Auth/Login/loginController.js' 
-
-export const routes = { 
-  Login: {
-    path: `Auth/Login/index.html`, 
-    controlador: loginController,
-    private: false,
-  },
-};
