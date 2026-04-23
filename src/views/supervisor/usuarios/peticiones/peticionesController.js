06a04f2bdb929b87dcbb94281729ef5acb4a9c25
Valentina Serrano <Valentinaserrano120@gmail.com>
2026-04-23 17:45:42 -0500

    ajuste de innerHTML a createElement con DOM, se elimino el stylefuide ya que no se usa y genera confusion, al igual que se reviso modulo de registro y no funciona, por el momento se cambio el nombre de una clase que estaba mal declarada

--- src/views/supervisor/usuarios/peticiones/peticionesController.js
+++ src/views/supervisor/usuarios/peticiones/peticionesController.js
@@ -31,34 +31,114 @@ export default async () => {
 
     // Fabricante dinámico de las tarjetas DOM a inyectar al Container Parent
     const carta = async (info) => {
-        const div = document.createElement("div"); // Receptáculo de memoria
-        div.classList.add("verUsuario"); // Base model styling CSS
+        const div = document.createElement("div"); // Contenedor principal
+        div.classList.add("verUsuario"); // clase del contenedor
 
-        // Interpolación literaria construyendo cada campo del solicitante (Dato Crudo DB Object 'info')
-        div.innerHTML = `
-            <div class="verUsuario__documento">
-                <i class="ri-id-card-line"></i>${info.document_number}
-            </div>
-            <div class="verUsuario__rol"><span>Peticion</span></div>
-            <div class="verUsuario__nombre">
-                <i class="ri-jewelry-line"></i>${info.full_name}
-            </div>
-            <div class="verUsuario__correo">
-                <i class="ri-mail-line"></i>${info.email}
-            </div>
-            <div class="verUsuario__seccional">
-                <i class="ri-team-line"></i>${info.sectional}
-            </div>
-            <div class="verUsuario__organizacion">
-                <i class="ri-parent-line"></i>${info.organization}
-            </div>
-            <div class="verUsuario__botones">
-                <button class="boton boton--azul verUsuario__botonPeticion"
-                        data-id="${info.id}">
-                    Ver informacion
-                </button>
-            </div>
-        `;
+        // hijo 1
+        const usuarioDocCont = document.createElement("div");
+        usuarioDocCont.classList.add('verUsuario__documento');
+
+        const iconoDoc = document.createElement('i');
+        iconoDoc.classList.add("ri-id-card-line");
+        const numberDoc = document.createElement("div")
+        numberDoc.textContent = info.document_number;
+
+        usuarioDocCont.append(iconoDoc, numberDoc);
+
+        //hijo 2
+        const usuarioRolCont = document.createElement("div");
+        usuarioRolCont.classList.add('verUsuario__rol');
+
+        const tituloRol = document.createElement('span');
+        tituloRol.classList.add("titulo__rol")
+        
+        usuarioRolCont.append(tituloRol);
+
+        //hijo 3
+        const usuarioNombreCont = document.createElement("div");
+        usuarioNombreCont.classList.add("verUsuario__nombre")
+
+        const iconoNom = document.createElement('i');
+        iconoNom.classList.add("ri-jewelry-line");
+        const fullName = document.createElement("div");
+        fullName.textContent =info.full_name;
+
+        usuarioNombreCont.append (iconoNom, fullName);
+
+        //hijo 4
+        const usuarioCorreoCont = document.createElement("div");
+        usuarioCorreoCont.classList.add("verUsuario__correo")
+
+        const iconoCorr = document.createElement('i');
+        iconoCorr.classList.add("ri-mail-line");
+        const email = document.createElement("div");
+        email.textContent =info.email;
+
+        usuarioCorreoCont.append (iconoCorr, email);
+
+        //hijo 5
+        const usuarioSeccionalCont = document.createElement("div");
+        usuarioSeccionalCont.classList.add("verUsuario__seccional");
+
+        const iconoSec = document.createElement('i');
+        iconoSec.classList.add("ri-team-line");
+        const seccional = document.createElement("div");
+        seccional.textContent =info.sectional;
+
+        usuarioSeccionalCont.append (iconoSec, seccional);
+
+        //hijo 6
+        const usuarioOrgCont = document.createElement("div");
+        usuarioOrgCont.classList.add("verUsuario__organizacion");
+
+        const iconoOrg = document.createElement("i");
+        iconoOrg.classList.add("ri-parent-line");
+        const org = document.createElement("div");
+        org.textContent = info.organization;
+
+        usuarioOrgCont.append(iconoOrg, org);
+
+        //hijo 7
+        const usuarioBtnCont = document.createElement("div");
+        usuarioBtnCont.classList.add("verUsuario__botones");
+
+        const btnCont = document.createElement("button");
+        btnCont.classList.add("boton", "boton--azul", "verUsuario__botonPeticion");
+        btnCont.setAttribute("data-id", info.id);
+        btnCont.textContent = "Ver informacion";
+
+        usuarioBtnCont.appendChild(btnCont);
+
+
+        //Agregar al contenedor principal
+        div.append(usuarioDocCont, usuarioRolCont, usuarioNombreCont, usuarioCorreoCont, usuarioSeccionalCont, usuarioOrgCont, usuarioBtnCont);
+
+        // div.innerHTML = `
+        //     <div class="verUsuario__documento">
+        //         <i class="ri-id-card-line"></i>${info.document_number}
+        //     </div>
+        //     <div class="verUsuario__rol">
+        //         <span>Peticion</span>
+        //     </div>
+        //     <div class="verUsuario__nombre">
+        //         <i class="ri-jewelry-line"></i>${info.full_name}
+        //     </div>
+        //     <div class="verUsuario__correo">
+        //         <i class="ri-mail-line"></i>${info.email}
+        //     </div>
+        //     <div class="verUsuario__seccional">
+        //         <i class="ri-team-line"></i>${info.sectional}
+        //     </div>
+        //     <div class="verUsuario__organizacion">
+        //         <i class="ri-parent-line"></i>${info.organization}
+        //     </div>
+        //     <div class="verUsuario__botones">
+        //         <button class="boton boton--azul verUsuario__botonPeticion"
+        //                 data-id="${info.id}">
+        //             Ver informacion
+        //         </button>
+        //     </div>
+        // `;
 
         return div; // Entrega la carta completa texturizada
     };
