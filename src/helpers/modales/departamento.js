8544daccbc7564e5f9f0ad99455e0099953bf60a
Oscar Solano <mao090997@gmail.com>
2026-06-17 17:25:52 -0500

    refactor: migracion de template HTML a Dom
    reemplazar string html inyectado por document.createElement

--- src/helpers/modales/departamento.js
+++ src/helpers/modales/departamento.js
@@ -14,24 +14,41 @@ import * as validacion from "../validacionInputs";
 // Crea y levanta el cuadro de diálogo con un formulario vacío 
 export const crear = async (recargarContainer) => {
 
-    const htmlModal = `
-        <div class="explicacion modal">
-            <p class="explicacion__titulo">Crear Departamento</p>
-        </div>
+    const explicacionDiv = document.createElement("div");
+    explicacionDiv.classList.add("explicacion", "modal");
 
-        <div class="form">
-        <div class="input">
-            <div class="form__inputBox modal-50">
-                <i class="ri-map-2-line"></i>
-                <input type="text" class="form__input form__nombre" 
-                placeholder="Nombre del departamento"autocomplete="off"
-                data-tipo="textoCorto">
-            </div>
-        </div>
-        </div>
-    `;
+    const tituloP = document.createElement("p");
+    tituloP.classList.add("explicacion__titulo");
+    tituloP.textContent = "Crear Departamento";
+    explicacionDiv.appendChild(tituloP);
 
-    alerta.Crear(htmlModal, async () => {
+    const formDiv = document.createElement("div");
+    formDiv.classList.add("form");
+
+    const inputDiv = document.createElement("div");
+    inputDiv.classList.add("input");
+
+    const inputBoxDiv = document.createElement("div");
+    inputBoxDiv.classList.add("form__inputBox", "modal-50");
+
+    const icon = document.createElement("i");
+    icon.classList.add("ri-map-2-line");
+
+    const input = document.createElement("input");
+    input.type = "text";
+    input.classList.add("form__input", "form__nombre");
+    input.placeholder = "Nombre del departamento";
+    input.autocomplete = "off";
+    input.dataset.tipo = "textoCorto";
+
+    inputBoxDiv.append(icon, input);
+    inputDiv.appendChild(inputBoxDiv);
+    formDiv.appendChild(inputDiv);
+
+    const container = document.createElement("div");
+    container.append(explicacionDiv, formDiv);
+
+    alerta.Crear(container, async () => {
         const contenedor = document.querySelector(".form");
         const nombre = document.querySelector(".form__nombre").value;
         const booleanValidacion = validacion.validadorAutomatico.validarTodo(contenedor)
